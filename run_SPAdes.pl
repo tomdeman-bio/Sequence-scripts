2d9c3ef7088bcf2826364cce76edb887e9bc761d
tomdeman-bio <tjb.deman@gmail.com>
2016-04-15 12:13:43 -0400

    update run_SPAdes.pl

--- run_SPAdes.pl
+++ run_SPAdes.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-#Runs SPAdes assembler in batch
+#runs SPAdes assembler in batch
 #Make sure to put SPAdes in your PATH variable
 #Written by Tom de Man
 
@@ -14,25 +14,25 @@ my %paired_files;
 foreach my $file (@files){
     my ($file_name,$dir)=fileparse($file);
     if($file_name =~ /(.+)_R([1|2])_/){
-	$paired_files{$1}[$2-1]=$file;
-    #attempt different naming scheme
+		$paired_files{$1}[$2-1]=$file;
+    	#attempt different naming scheme
     }elsif($file_name =~ /(.+)_([1|2])/){
-	$paired_files{$1}[$2-1]=$file;
+		$paired_files{$1}[$2-1]=$file;
     }else{
-	warn "Input file does not contain '_R1_' or '_R2_' in name: $file";
+		warn "Input file does not contain '_R1_' or '_R2_' in name: $file";
     }
 }
 
 foreach my $name (sort keys %paired_files){
     unless(defined($paired_files{$name}[0]) && defined($paired_files{$name}[1])){
-	warn "Couldn't find matching paired end files for file starting with: $name";
-	next;
+		warn "Couldn't find matching paired end files for file starting with: $name";
+		next;
     }
     print "assembling your data....\n";
     print "----------------------\n";
     print "$paired_files{$name}[0]"." <--> "."$paired_files{$name}[1]"."\n";
 
-    my $cmd="spades.py -t 16 --only-assembler -1 $paired_files{$name}[0] -2 $paired_files{$name}[1] -o $paired_files{$name}[0]_assembly";
+    my $cmd="spades.py -t 16 --careful --only-assembler -1 $paired_files{$name}[0] -2 $paired_files{$name}[1] -o $paired_files{$name}[0]_assembly";
     print $cmd,"\n";
     die if system($cmd);
 }
