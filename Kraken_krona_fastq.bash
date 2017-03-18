86b8d254f9233505a03e99c28b6789db54cd137c
tomdeman-bio <tjb.deman@gmail.com>
2017-03-18 14:48:45 -0400

    add Kraken/Krona script

---
+++ Kraken_krona_fastq.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+kraken_db="/path/to/custom/Kraken-RefSeq/database/"
+
+for file in *_R1_001.fastq
+
+do
+	b=$(basename $file _R1_001.fastq)
+	rev=${b}_R2_001.fastq
+
+	echo "[:] Analyzing $file and $rev"
+
+	kraken --version
+
+	echo "[:] Running kraken.  Output: $file.kraken / $file.classified"
+
+	kraken --paired --db $kraken_db --preload --threads 12 --output $file.kraken --classified-out $file.classified $file $rev
+
+	echo "[:] Generating metaphlan compatible report."
+
+	kraken-mpa-report --db $kraken_db $file.kraken >> $file.mpa
+
+	echo "[:] Generating krona output for $file."
+
+	python metaphlan2krona.py -p $file.mpa -k $file.krona
+
+	ktImportText -o all_output.html *.krona 
+
+done
